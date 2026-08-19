e5e26c9a713ca28afb176459fc7d523d893daae4
rishafa61 <rishafa61@gmail.com>
2026-08-19 20:12:34 +0700

    fix: add break-system-packages flag for vercel build

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building project..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
