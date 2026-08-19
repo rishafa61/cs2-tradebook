74fca38404a3fe8c382f851bb7bad05a7d2cb405
rishafa61 <rishafa61@gmail.com>
2026-08-19 20:15:04 +0700

    fix: use /tmp directory for sqlite database on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Building project..."
 python3 -m pip install -r requirements.txt --break-system-packages
+python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
--- tradebook_project/settings.py
+++ tradebook_project/settings.py
@@ -58,10 +58,12 @@ WSGI_APPLICATION = 'tradebook_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+IS_VERCEL = os.environ.get('VERCEL') == '1'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3' if IS_VERCEL else BASE_DIR / 'db.sqlite3',
     }
 }
 
